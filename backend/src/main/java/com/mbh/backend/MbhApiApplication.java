e2e5c69b78ddf005fa3e08ca867e5f8978896788
James Kelly <jk377y@gmail.com>
2026-02-11 11:52:19 -0600

    generated spring boot project for backend; starter files only on this commit

---
+++ backend/src/main/java/com/mbh/backend/MbhApiApplication.java
@@ -0,0 +1,13 @@
+package com.mbh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbhApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mbh/backend/MbhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
