b6b63b237eb0d0f565daea534072589c423e47b8
James Kelly <jk377y@gmail.com>
2026-02-13 23:38:03 -0600

    added corsconfig to connect frontend and backend live deployment endpoints; created dockerfile; edited application.properties to allow for env variables to load correctly

---
+++ backend/src/main/java/com/mbh/backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.mbh.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://www.masterblasterhub.com") // live url
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
