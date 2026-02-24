446fdf24adb8bf6d4feaf835e1d34484b630ce4e
James Kelly <jk377y@gmail.com>
2026-02-24 06:30:48 -0600

    edited footer text

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.footerTop}>© 2026 Master Blaster Hub</div>
-            <div className={styles.footerBottom}>Built with React & Spring Boot by James Kelly</div>
+            <div className={styles.footerBottom}>WGU Software Engineering Capstone by James Kelly</div>
             <NavLink to="/tech-stack" className={styles.footerLink}>
                 <span>Technology Stack and Deployment</span>
             </NavLink>
